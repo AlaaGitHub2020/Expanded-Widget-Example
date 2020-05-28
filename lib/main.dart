296def286f97f935c7791e2ad2876664d282b325
Alaa Mashkouk <alaamashkouk2@gmail.com>
2020-05-28 14:49:35 +0530

    Expanded Widget example

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:expandedexample/my_home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Expanded Simple Example',
+    home: MyHome(),
+  ));
+}
